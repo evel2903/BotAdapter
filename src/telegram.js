b6644c5a836d7f4c9bb0ee33e2c60e001d2830a7
SangLx <sanglx@k-s-s.com.vn>
2024-06-12 12:57:31 +0700

    using axios

--- src/telegram.js
+++ src/telegram.js
@@ -1,21 +1,25 @@
 import 'dotenv/config'
-import {formatVNDateTime} from'./common.js'
+import axios from 'axios';
+import { formatVNDateTime } from './common.js';
 
 // Function to send a message to a Telegram channel
-export const sendTelegramMessage =  async (message) => {
+export const sendTelegramMessage = async (message) => {
     try {
-        const token = process.env.TELEGRAM_TOKEN
-        const channel = process.env.TELEGRAM_CHANNEL
-        message = `[${formatVNDateTime(new Date())}]: ${message}`
+        const token = process.env.TELEGRAM_TOKEN;
+        const channel = process.env.TELEGRAM_CHANNEL;
+        message = `[${formatVNDateTime(new Date())}]: ${message}`;
         // Construct the Telegram API endpoint for sending a message
-        const request = await fetch(`https://api.telegram.org/${token}/sendMessage?chat_id=${channel}&text=${message}`, {
-            method: 'GET',
-            redirect: 'follow'
+        const url = `https://api.telegram.org/${token}/sendMessage`;
+        
+        // Use axios to send the GET request
+        const response = await axios.get(url, {
+            params: {
+                chat_id: channel,
+                text: message
+            }
         });
-        // Parse the JSON response from the Telegram API
-        const response = await request.json();
-        // Return the response object
-        return response;
+        // Return the response data
+        return response.data;
     } catch (error) {
         // Handle errors by logging them to the console
         console.error('Error:', error);
